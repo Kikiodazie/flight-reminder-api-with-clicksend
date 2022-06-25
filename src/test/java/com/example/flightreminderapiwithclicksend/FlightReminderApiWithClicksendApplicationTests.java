c13c35d4465b96010b1bf5fd22df017eb4a4cac6
kikiodazie <dodazie@gmail.com>
2022-06-25 16:35:30 +0100

    Initial commit

---
+++ src/test/java/com/example/flightreminderapiwithclicksend/FlightReminderApiWithClicksendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightreminderapiwithclicksend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReminderApiWithClicksendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
